2ec719b67b3d25b2d9f2f4bf3d973f3f804bdd46
root <root@vmi3129025.contaboserver.net>
2026-06-26 01:13:47 +0200

    [asw] fix. delete data sql

--- query/delete_data.sql
+++ query/delete_data.sql
@@ -61,3 +61,87 @@ TRUNCATE TABLE
     freelancer,
     users
 RESTART IDENTITY CASCADE;
+
+-- DROP TABLE IF EXISTS
+--     review_ai_analysis,
+--     review_skill_tags,
+--     review_written_content,
+--     review_ratings,
+--     bias_detection_log,
+--     red_flag_alerts,
+--     trust_score_history,
+--     freelancer_trust_scores,
+--     freelancer_performance_scores,
+--     ai_review_prompts,
+--     reviews,
+--
+--     dm_message_attachment,
+--     dm_message,
+--     dm_thread,
+--
+--     contract_submission_file,
+--     contract_submission,
+--
+--     portfolio_embedding,
+--     contract_embedding,
+--     job_role_embedding,
+--     freelancer_embedding,
+--
+--     email_verification_otps,
+--     password_reset_otps,
+--     refresh_tokens,
+--     user_oauth_providers,
+--
+--     harmful_text_queue,
+--     scam_job_flags,
+--     client_scam_record,
+--     user_reports,
+--     report_auto_actions,
+--     appeals,
+--
+--     notifications,
+--
+--     client_trust_score,
+--     performance_rating,
+--     rating,
+--     saved_job,
+--     contract_terms,
+--     portfolio,
+--     contract,
+--     proposal_file,
+--     proposal,
+--     job_file,
+--     job_role_skill,
+--     job_role,
+--     job_post,
+--     work_experience,
+--     education,
+--     freelancer_skill,
+--     skill,
+--
+--     client,
+--     freelancer,
+--     users,
+--     testing_table,
+--     message_attachment
+-- CASCADE;
+--
+-- DROP TYPE IF EXISTS
+--     rate_time_type,
+--     skill_category_type,
+--     proficiency_skill,
+--     project_type,
+--     project_scope,
+--     experience_level,
+--     job_status,
+--     budget_type,
+--     importance_level,
+--     proposal_status,
+--     payment_structure,
+--     contract_status,
+--     review_status,
+--     review_sentiment_label,
+--     review_alert_type,
+--     review_alert_severity,
+--     trust_snapshot_reason
+-- CASCADE;
